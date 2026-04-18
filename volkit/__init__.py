17374f92f75bd20798f48463d9ca2d5b6a3bb239
fedecarz <federico.carzaniga@gmail.com>
2026-04-18 12:24:43 +0200

    Update __init__.py

--- volkit/__init__.py
+++ volkit/__init__.py
@@ -4,3 +4,8 @@ Options pricing, IV surface, and vol spread analysis.
 """
 
 __version__ = "0.1.0"
+
+from volkit import market_data
+from volkit.black_scholes import bs_price, greeks
+from volkit.iv_surface import plot_surface, build_iv_surface, implied_volatility
+from volkit.vol_spread import analyze_vol_spread, realized_vol, atm_implied_vol, compute_vol_spread
